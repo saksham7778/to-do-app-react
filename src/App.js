563e660d65e3a9bfe5f287814a45c5fc7d958565
saksam agarwal <saksamagr@gmail.com>
2020-04-28 17:33:10 +0530

    added static fields

--- src/App.js
+++ src/App.js
@@ -2,25 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+
+
+  render() {
+    return (
+      <div>
+        <img src={logo} width="100" height="100" alt="app-logo" className="logo" />
+        <h1 className="app-title">ToDo App</h1>
+       
+        <div className="container">
+          Add an Item....
+          <br />
+          <input
+            type="text"
+            className="input-text"
+            placeholder="Write a Todo"
+            required/>
+
+          <button
+            className="add-btn">
+            Add Todo
+          </button>
+          
+          <div className="list">
+            <ul>
+              <li>
+                <input type="checkbox" name="" id="" />
+                Record youtube videos
+                <button className="btn">Delete</button>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+      
+      </div>
+    );
+  }
+
 }
 
 export default App;
