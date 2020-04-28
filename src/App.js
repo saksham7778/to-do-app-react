dcd14b2f472d22adeb5280d4fc54165255c413dc
saksam agarwal <saksamagr@gmail.com>
2020-04-28 17:44:43 +0530

    made helper functions

--- src/App.js
+++ src/App.js
@@ -4,9 +4,44 @@ import './App.css';
 
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: []
+    };
+  }
+
+  addItem(todoValue) {
+    if (todoValue !== "") {
+      const newItem = {
+        id: Date.now(),
+        value: todoValue,
+        isDone: false
+      };
+      const list = [...this.state.list];
+      list.push(newItem);
+
+      this.setState({
+        list:list,
+        newItem: ""
+      });
+    }
+  }
+
+  deleteItem(id) {
+    const list = [...this.state.list];
+    const updatedlist = list.filter(item => item.id !== id);
+    this.setState({ list: updatedlist });
+  }
+
+  updateInput(input) {
+    this.setState({ newItem: input });
+  }
 
 
   render() {
+
     return (
       <div>
         <img src={logo} width="100" height="100" alt="app-logo" className="logo" />
