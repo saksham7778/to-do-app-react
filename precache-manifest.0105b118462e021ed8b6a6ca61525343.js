1965cf3a320f782d4fc157b42f7a4ede86c56efa
saksam agarwal <saksamagr@gmail.com>
2020-05-07 02:34:20 +0530

    Updates

---
+++ precache-manifest.0105b118462e021ed8b6a6ca61525343.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea2455718cd684373038bbeb56a50af7",
+    "url": "/to-do-app-react/index.html"
+  },
+  {
+    "revision": "f3c1aac6426bcb797366",
+    "url": "/to-do-app-react/static/css/main.427cb435.chunk.css"
+  },
+  {
+    "revision": "1fcef90402de53a1b807",
+    "url": "/to-do-app-react/static/js/2.9225389d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-app-react/static/js/2.9225389d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c1aac6426bcb797366",
+    "url": "/to-do-app-react/static/js/main.c62e0779.chunk.js"
+  },
+  {
+    "revision": "4320ed3b9bb8d9e9e608",
+    "url": "/to-do-app-react/static/js/runtime-main.a3c351db.js"
+  },
+  {
+    "revision": "43a3784fb686f4c349f80f4d1aa175b9",
+    "url": "/to-do-app-react/static/media/to-do2.43a3784f.jpg"
+  }
+]);
